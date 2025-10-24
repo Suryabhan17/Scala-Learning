45a15998dc1057352efdcd66467b6c78b9ee9880
Suryabhan17 <suryabhanuday99@gmail.com>
2025-10-24 15:08:27 +0530

    using Custom Margin Charcter

--- src/main/scala/MultilineString.scala
+++ src/main/scala/MultilineString.scala
@@ -8,6 +8,19 @@ object MultilineString {
         |My name is Suryabhan Singh
         |""".stripMargin
         println(text)
+
+    val text1 =
+      """
+           this is a multiline string.
+           It keeps space and indentation as it is.
+           No stripMargin used. """
+           println(text1)
+    val cmc =
+      """
+        #Hello
+        #This is scala
+        #Using custom margin character""".stripMargin('#')
+        println(cmc)
   }
 
 }
